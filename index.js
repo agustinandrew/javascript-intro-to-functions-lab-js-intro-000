caf5d2817ec309126a6041b77b6a16dbd9bad9ad
pragmatic-founder-4933 <agustinandrew@gmail.com>
2020-07-22 23:32:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,4 @@ function sayHiToGrandma(string)
 
 if (string.toUpperCase() === string) {return "YES INDEED!"}
 
-if (string === "I love you, Grandma.!") {return "I love you, too"}
-
-return "Are you eating enough?"}
+if (string=== "I love you, Grandma.!") {return "I love you, too"}}
