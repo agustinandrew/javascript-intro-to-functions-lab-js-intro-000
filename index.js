54d89d6ad775c62aaf55c73a0ff32e55bd0606c0
pragmatic-founder-4933 <agustinandrew@gmail.com>
2020-07-22 23:27:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string)
 function sayHiToGrandma(string)
 {if (string.toLowerCase() === string) {return "I can't hear you!"}
 
-if (string.toUpperCase() === string) {return "Yes INDEED!"}
+if (string.toUpperCase() === string) {return "YES INDEED!"}
 
 if (string === "I love you, Grandma!") {return "I love you, too"}
 }
